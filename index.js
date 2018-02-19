03fb4393fe0e386db92be45db5aa6ec3b54b6f0d
Austin Kenny <austin.kenny@etudiant.univ-reims.fr>
2018-02-19 17:23:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const figlet = require('figlet');
+
+
+axios
+    .get(
+    'http://www.citura.fr/fr/NextDeparture/logicalstop?logicalId=1210'
+    )
+    .then(res => {
+        const $ = cheerio.load(res.data);
+        let depart = $('.list-next-departure-link').text();
+        let trimDepart = depart.replace(/  +/g, '').replace(/\n\n+/g, ' ').replace(/Bus/g, '');
+        if (!trimDepart) {
+            trimDepart = 'Pas de prochain départ'
+        }
+        figlet('ligne 3', function (err, data) {
+            if (err) {
+                console.log('Something went wrong...');
+                console.dir(err);
+                return;
+            }
+            console.log(data)
+            console.log(trimDepart);
+        });
+    })
+    .catch(err => console.log(err));
---
+++ readme.md
@@ -0,0 +1,3 @@
+
+This is a simple CLI that displays when the next bus on the line I usually take will arrive.
+# Not meant for external use
